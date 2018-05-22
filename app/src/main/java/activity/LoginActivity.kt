19a5b364d2a4ee605142847e089810b99e50e9b6
xudehua <xdhwwdz2011@163.com>
2018-05-22 10:56:39 +0800

    登陆Activity

---
+++ app/src/main/java/activity/LoginActivity.kt
@@ -0,0 +1,14 @@
+package activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class LoginActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+
+    }
+}
